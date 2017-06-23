28fe2b973bda1b828e4893adb7822df9917a93a9
SWATHY SUDARSANAN <04914@user>
2017-06-23 11:39:14 -0400

    sort

--- insertion.c
+++ insertion.c
@@ -1,25 +1,40 @@
 #include<stdio.h>
 int sort(int *array)
 { 
-   for(int i=1;i<7;i++)
+ int i;
+   for(i=1;i<7;i++)
      {
       int temp=array[i];
       int j=i-1;
-      for(;j>0;array[j]>temp;j--)
+      while(j>=0&&array[j]>temp)
      {
       array[j+1]=array[j];
+      j=j-1;
      }
        array[j+1]=temp;
   }
- return 0;
+}
+int read()
+{
+ int n,i,array[20];
+printf("enter the no.of elements");
+scanf("%d",&n);
+printf("enter elements");
+for(i=0;i<n;i++)
+ {
+   scanf("%d",&array[i]);
+ }
 }
 int main()
  {
-    int array[7]={4,23,43,38,15,10,30;
-    
+   int array[20],i,n;
+    //int array[7]={4,23,43,38,15,10,30};
+ 
+    read();
     sort(array);
+    printf("sorted list in ascending order\n");
   
-  for(i=0;i<7;i++)
+  for(i=0;i<n;i++)
    {
       printf("%d\n",array[i]);
    }
